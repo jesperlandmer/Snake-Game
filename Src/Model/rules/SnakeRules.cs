521dbca30b68fdd09e25f2f8081991f9ac27a4d9
[Jesper] <[jp222vq@student.lnu.se]>
2017-11-24 16:20:51 +0100

    GREEN

--- Src/Model/rules/SnakeRules.cs
+++ Src/Model/rules/SnakeRules.cs
@@ -5,9 +5,24 @@ namespace Snake_Game.Src.Model.rules
 {
     class SnakeRules : ISnakeRules
     {
+        private int _squareArenaLimit = 30;
+
         public bool IsGameOver(Snake m_snake)
         {
-            throw new NotImplementedException();
+            if (HitHeightLimits(m_snake.GetHead()) || HitLengthLimits(m_snake.GetHead()))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool HitHeightLimits(Position p)
+        {
+            return (p.YCoordinate <= 0 || p.YCoordinate >= _squareArenaLimit);
+        }
+        private bool HitLengthLimits(Position p)
+        {
+            return (p.XCoordinate <= 0 || p.XCoordinate >= _squareArenaLimit);
         }
     }
 }
